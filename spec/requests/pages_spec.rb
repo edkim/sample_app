0e8c29739486793fdb2d654aa7e52f351b14bf59
Edward Kim <edk217@gmail.com>
2012-01-30 22:29:54 -0500

    remove default rails page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,27 +4,27 @@ describe "pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/pages/home'
-      page.should have_content('Sample App')
+ it "should have the h1 'Sample App'" do
+      visit root_path
+      page.should have_selector('h1', text: 'Sample App')
     end
-    
+
     it "should have the title 'Home'" do
-      visit '/pages/home'
+      visit root_path
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        text: "Ruby on Rails Tutorial Sample App | Home")
     end
   end
   
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     
     it "should have the title 'Help'" do
-      visit '/pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | Help")
     end
@@ -33,14 +33,28 @@ describe "pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
     
     it "should have the title 'About Us'" do
-      visit '/pages/about'
+      visit about_path
       page.should have_selector('title',
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
-    end                  
+    end    
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
 end
